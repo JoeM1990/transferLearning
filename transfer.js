1f0f04547e3bf0872a87e6b56bb0cc95043a3596
Joe Monkila <jnkiwa25@gmail.com>
2023-08-04 14:13:42 +0100

    script

--- transfer.js
+++ transfer.js
@@ -1,18 +1,5 @@
 /**
  * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
  */
 
 const STATUS = document.getElementById('status');
@@ -170,7 +157,7 @@ function dataGatherLoop() {
 
     STATUS.innerText = '';
     for (let n = 0; n < CLASS_NAMES.length; n++) {
-      STATUS.innerText += CLASS_NAMES[n] + ' data count: ' + examplesCount[n] + '. ';
+      STATUS.innerText += CLASS_NAMES[n] + ' nombre echantillon: ' + examplesCount[n] + '. ';
     }
 
     window.requestAnimationFrame(dataGatherLoop);
@@ -223,7 +210,7 @@ function predictLoop() {
       let prediction = model.predict(imageFeatures.expandDims()).squeeze();
       let highestIndex = prediction.argMax().arraySync();
       let predictionArray = prediction.arraySync();
-      STATUS.innerText = 'Prediction: ' + CLASS_NAMES[highestIndex] + ' with ' + Math.floor(predictionArray[highestIndex] * 100) + '% confidence';
+      STATUS.innerText = 'Prediction: ' + CLASS_NAMES[highestIndex] + ' with ' + Math.floor(predictionArray[highestIndex] * 100) + '% exactitude';
     });
 
     window.requestAnimationFrame(predictLoop);
@@ -244,7 +231,7 @@ function reset() {
   }
   trainingDataInputs.splice(0);
   trainingDataOutputs.splice(0);
-  STATUS.innerText = 'No data collected';
+  STATUS.innerText = 'Aucune donnée...';
   
   console.log('Tensors in memory: ' + tf.memory().numTensors);
 }
